0b6492bc3f7d753ddff51e2a1d078a882b788260
Kevin Patel <kevnpatel99@gmail.com>
2024-10-31 19:02:45 -0400

    Completed Object Basics Exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const cleanStr = str.replace(/[\W_]/g, '').toLowerCase();
+
+    for (let i = 0; i < cleanStr.length; i++) { 
+        console.log(cleanStr[i], cleanStr[cleanStr.length - 1 - i]);
+        if (cleanStr[i] != cleanStr[cleanStr.length - 1 - i])  {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = null;
+    let oldestAge = 0;
 
+    for (const person of people) { 
+        
+        if (person.yearOfDeath == undefined) { 
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        
+        if ((person.yearOfDeath - person.yearOfBirth) > oldestAge) {
+            oldest = person;
+            oldestAge = person.yearOfDeath - person.yearOfBirth;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
